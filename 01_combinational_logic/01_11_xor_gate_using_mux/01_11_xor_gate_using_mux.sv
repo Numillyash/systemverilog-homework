cd95c32e7af27ca2c28e66d84bed164ed0cb0e25
Numillyash <Numillyash-std@yandex.ru>
2025-10-26 21:51:33 +0300

    fix comments

--- 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
+++ 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
@@ -27,18 +27,13 @@ module xor_gate_using_mux
   // Task:
   // Implement xor gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
-  wire nb;  // ~b, получаем тоже через mux
-
-  // Инвертор на mux: ~b = mux(1, 0, b)
+  wire nb;
   mux m_notb (
         .d0 (1'b1),
         .d1 (1'b0),
         .sel(b),
         .y  (nb)
       );
-
-  // XOR: если a=0 -> выдать b, если a=1 -> выдать ~b
-  // o = mux(b, ~b, a)
   mux m_xor (
         .d0 (b),
         .d1 (nb),
